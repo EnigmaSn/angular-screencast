bcbe5b15cf1f8bfaf0039d73bf7a49e184a0aac9
Anna Varaksina <enigmasnape@gmail.com>
2022-03-13 19:27:30 +0300

    Использование Хуков

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.less']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent 
+  implements 
+    OnChanges,
+    OnInit
+{
+  @Input() navTitle: string = '';
+
   showFiller = false;
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
 
   ngOnInit(): void {
   }
